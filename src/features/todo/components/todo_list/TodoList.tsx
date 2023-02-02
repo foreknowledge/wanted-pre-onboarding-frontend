eea7ad4f6c90447c7d18ed47a998eaea0ba98a4f
foreknowledge <kyj_0129@naver.com>
2023-02-02 16:41:24 +0900

    feat: todo list 서버 데이터 보여주기

--- src/features/todo/components/todo_list/TodoList.tsx
+++ src/features/todo/components/todo_list/TodoList.tsx
@@ -1,10 +1,22 @@
+import { useContext, useEffect, useState } from 'react';
+import { getTodos } from '../../../../apis/todo';
+import TokenContext from '../../../../context/token/TokenContext';
 import TodoData from '../../types/TodoData.types';
 import TodoItem from '../todo_item/TodoItem';
 
 const TodoList = () => {
+  const { token } = useContext(TokenContext);
+  const [todos, setTodos] = useState<TodoData[]>([]);
+
+  useEffect(() => {
+    if (token) {
+      getTodos(token).then(setTodos);
+    }
+  }, [token]);
+
   return (
     <ul className="m-8">
-      {SAMPLE_TODOS.map((todo) => (
+      {todos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} />
       ))}
     </ul>
@@ -12,48 +24,3 @@ const TodoList = () => {
 };
 
 export default TodoList;
-
-const SAMPLE_TODOS: TodoData[] = [
-  {
-    id: 1,
-    todo: '과제하기1',
-    isCompleted: true,
-    userId: 1,
-  },
-  {
-    id: 2,
-    todo: '과제하기2',
-    isCompleted: false,
-    userId: 1,
-  },
-  {
-    id: 3,
-    todo: '과제하기3',
-    isCompleted: true,
-    userId: 1,
-  },
-  {
-    id: 4,
-    todo: '과제하기4',
-    isCompleted: true,
-    userId: 1,
-  },
-  {
-    id: 5,
-    todo: '과제하기5',
-    isCompleted: false,
-    userId: 1,
-  },
-  {
-    id: 6,
-    todo: '과제하기6',
-    isCompleted: false,
-    userId: 1,
-  },
-  {
-    id: 7,
-    todo: '과제하기7',
-    isCompleted: false,
-    userId: 1,
-  },
-];
