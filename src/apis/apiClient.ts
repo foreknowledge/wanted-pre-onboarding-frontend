abcb6b1227b87d289c9441755ac41bfa5361e0e0
foreknowledge <kyj_0129@naver.com>
2023-02-04 22:31:46 +0900

    feat: api 요청 로컬 -> 원격 서버로 변경

--- src/apis/apiClient.ts
+++ src/apis/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://pre-onboarding-selection-task.shop/',
 });
 
 export default apiClient;
