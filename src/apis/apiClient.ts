0a4e27f7d55b3269fbc46ae9c8d3d4f94e64aef2
foreknowledge <kyj_0129@naver.com>
2023-05-26 18:25:34 +0900

    서버 URL 주소 변경

--- src/apis/apiClient.ts
+++ src/apis/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://pre-onboarding-selection-task.shop/',
+  // baseURL: 'https://pre-onboarding-selection-task.shop/',
+  baseURL: 'http://localhost:8000',
 });
 
 export default apiClient;
